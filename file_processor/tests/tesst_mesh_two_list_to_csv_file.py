c937fe80add284e787694d74244d0b8e6d9f3b84
Aleksei Tsypov <al.tsypov@gmail.com>
2025-10-18 20:59:39 +0300

    test

---
+++ file_processor/tests/tesst_mesh_two_list_to_csv_file.py
@@ -0,0 +1,21 @@
+import csv
+import sys
+import os
+
+
+from file_processor.file_processor import mesh_two_list_to_csv_file
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__) + "/.."))
+
+
+def test_mesh_two_list_to_csv_file():
+    list1 = ['A1', 'A2', 'A3']
+    list2 = ['B1', 'B2', 'B3']
+    filename = 'test_output.csv'
+
+    mesh_two_list_to_csv_file(list1, list2, filename)
+    print("hre")
+
+    with open(filename, mode='r', newline='') as file:
+        reader = csv.reader(file)
+        for row, expected in zip(reader, zip(list1, list2)):
+            assert row == list(expected), f"Row {row} does not match expected {expected}"
